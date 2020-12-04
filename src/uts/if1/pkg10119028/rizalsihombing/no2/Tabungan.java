74825cdb4a4c2e9c7d7982922a87db950444f191
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-12-04 21:39:08 +0700

    Add files via upload

---
+++ src/uts/if1/pkg10119028/rizalsihombing/no2/Tabungan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if1.pkg10119028.rizalsihombing.no2;
+
+/**
+ *
+ * @author RizalSihombing
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk menampilkan tabungan seorang user
+ */
+public class Tabungan {
+    private int saldo;
+    
+    public Tabungan(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    public int ambilUang(int jumlah) {
+        return saldo - jumlah;
+    }
+}
---
+++ src/uts/if1/pkg10119028/rizalsihombing/no2/UTSIF110119028RIZALSIHOMBINGNo2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if1.pkg10119028.rizalsihombing.no2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author RizalSihombing
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk menampilkan tabungan seorang user
+ */
+public class UTSIF110119028RIZALSIHOMBINGNo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("====Program Penarikan Uang====");
+        System.out.print("Masukkan Saldo Awal : ");
+        int saldo = scanner.nextInt();
+        Tabungan tabungan = new Tabungan(saldo);
+        System.out.print("Jumlah uang yang diambil : ");
+        int jumlah = scanner.nextInt();
+        
+        System.out.println("Saldo Anda Sekarang : " + tabungan.ambilUang(jumlah));
+    }
+    
+}
